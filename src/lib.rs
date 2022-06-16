7d14ef4fc32da9ab3fb9913f55d5a17ae5e3a0cc
ThalusA <15985204+ThalusA@users.noreply.github.com>
2022-06-16 22:31:05 +0200

    https://github.com/arcnmx/ddc-hi-rs/issues/4#issuecomment-1155387409

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@ use neon_bindings::{display_get_brightness, display_set_brightness, display_info
 #[cfg(feature = "node-bindings")]
 use neon::prelude::*;
 
-use ddc::{Ddc, DdcHost, VcpValue};
+use ddc::{Ddc, DdcHost, FeatureCode, VcpValue};
 use ddc_hi::Display;
 pub use ddc_hi::Query;
 
@@ -40,7 +40,7 @@ pub fn get_enhanced_display(query: Query, needs_caps: bool, id: usize) -> Result
 pub fn get_brightness(id: usize) -> Result<VcpValue, Error> {
     let mut display = get_enhanced_display(Query::Any, true, id)?;
     let result = display.info.mccs_database
-        .get(mccs::ImageAdjustment::Luminance.into())
+        .get(mccs::ImageAdjustment::Luminance as FeatureCode)
         .map(|feature| display.handle.get_vcp_feature(feature.code)
             .map_err(|error| Error::new(ErrorKind::TimedOut, error.to_string())))
         .or(Some(Err(Error::new(ErrorKind::Unsupported, "This display doesn't support brightness operations")))).unwrap();
@@ -51,7 +51,7 @@ pub fn get_brightness(id: usize) -> Result<VcpValue, Error> {
 pub fn set_brightness(id: usize, value: u16) -> Result<(), Error> {
     let mut display = get_enhanced_display(Query::Any, true, id)?;
     let result = display.info.mccs_database
-        .get(mccs::ImageAdjustment::Luminance.into())
+        .get(mccs::ImageAdjustment::Luminance as FeatureCode)
         .map(|feature| display.handle.set_vcp_feature(feature.code, value)
             .map_err(|error| Error::new(ErrorKind::TimedOut, error.to_string())))
         .or(Some(Err(Error::new(ErrorKind::Unsupported, "This display doesn't support brightness operations")))).unwrap();
